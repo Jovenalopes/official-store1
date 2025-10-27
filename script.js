f3e1118ce729b9731ce4a2fae40a0edc93cff81a
MundoLivre <dominio3002@gmail.com>
2025-10-27 13:37:02 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const button = document.getElementById("cta-button");
-    const baseLink = "https://prostapeak.com/?hopId=05de575f-8240-4b85-a9e5-1fe56fcff48b";
-
-    const params = window.location.search;
-    button.href = baseLink + params;
-});
